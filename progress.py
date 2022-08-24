e71e90d5ace22964de3f435fc619760e51ce33ef
Erastus Murungi <erastuswagwan@gmail.com>
2022-08-24 14:31:12 -0700

    added support for downloading actual files

--- progress.py
+++ progress.py
@@ -1,12 +1,17 @@
 import sys
 import uuid
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from itertools import cycle
 from math import ceil
 from random import randint
+from threading import Thread
 from time import monotonic, sleep
-from typing import TextIO, Iterable
+from typing import Iterable, Optional, TextIO
 
+import click
+import requests
+from humanfriendly import format_timespan
 from termcolor import colored
 
 ANSI_ERASE_CURRENT_LINE = "\x1b[2K"
@@ -16,9 +21,8 @@ ANSI_SHOW_CURSOR = "\x1b[?25h"
 
 PROGRESS_SPINNER_SEQUENCE = cycle("◐ ◓ ◑ ◒".split())
 
-
-COMPLETED_JOBS_REFRESH_TIME = 1.5
-FRAMES_PER_CYCLE = 1.0 / 30.0
+COMPLETED_JOBS_REFRESH_TIME = 3
+FRAMES_PER_CYCLE = 1.0 / 10.0
 
 MAX_NUMBER_CHARACTERS_HEADER_PROGRESS_BAR = 30
 
@@ -29,19 +33,13 @@ def get_green_bold_colored(text: str) -> str:
     return colored(f"{text}", "green", attrs=["bold"])
 
 
-class DeterminateProgressItem(ABC):
+class IndeterminateProgressItem(ABC):
     id: uuid.UUID
+    is_finished: bool = False
+    start_time: float = float("inf")
 
     def __init__(self):
         self.id = uuid.uuid4()
-
-    @abstractmethod
-    def get_normalized_progress(self) -> float:
-        """
-        Returns the progress of this time with 1 being complete
-
-        :return: The progress from 0 to 1
-        """
 
     @abstractmethod
     def is_completed(self) -> bool:
@@ -54,8 +52,8 @@ class DeterminateProgressItem(ABC):
     def is_not_completed(self):
         return not self.is_completed()
 
-    def get_percentage_progress(self) -> float:
-        return self.get_normalized_progress() * 100
+    def cleanup(self):
+        pass
 
     def get_progress_item_title(self) -> str:
         """
@@ -65,29 +63,55 @@ class DeterminateProgressItem(ABC):
         return str(self.id)
 
     def pretty_print_progress(self, text_io: TextIO = sys.stdout) -> None:
-        progress_item_title = self.get_progress_item_title()
+        progress_item_title = colored(self.get_progress_item_title(), "white")
         progress_spinner_code_point = next(PROGRESS_SPINNER_SEQUENCE)
         if self.is_completed():
             # we define done_str because f-strings do not allow `\`
-            done_str = "\u2714 Done!"
+            done_str = "\u2714"
             text_io.write(
                 f"{INDENT}{get_green_bold_colored(done_str)} [{progress_item_title}]\n"
             )
         else:
             text_io.write(
-                f"{INDENT}{progress_spinner_code_point} {self.get_percentage_progress():04.2f} % [{progress_item_title}]\n"
+                f"{INDENT}{progress_spinner_code_point} {progress_item_title} %\n"
+            )
+
+
+class DeterminateProgressItem(IndeterminateProgressItem):
+    finish_time: float
+
+    @abstractmethod
+    def get_normalized_progress(self) -> float:
+        """
+        Returns the progress of this time with 1 being complete
+
+        :return: The progress from 0 to 1
+        """
+
+    def get_percentage_progress(self) -> float:
+        return self.get_normalized_progress() * 100
+
+    def pretty_print_progress(self, text_io: TextIO = sys.stdout) -> None:
+        progress_item_title = colored(self.get_progress_item_title(), "white")
+        progress_spinner_code_point = next(PROGRESS_SPINNER_SEQUENCE)
+        if self.is_completed():
+            # we define done_str because f-strings do not allow `\`
+            done_str = "\u2714"
+            text_io.write(
+                f"{INDENT}{get_green_bold_colored(done_str)} {progress_item_title}... Finished in {format_timespan(self.finish_time - self.start_time)}\n"
+            )
+        else:
+            text_io.write(
+                f"{INDENT}{progress_spinner_code_point} {progress_item_title} ... {format_timespan(monotonic() - self.start_time)}    ({self.get_percentage_progress():04.2f} %)\n"
             )
 
 
 class MockDownload(DeterminateProgressItem):
-    start_time: float
-
     def __init__(self, download_size: float, bandwidth: float):
         super().__init__()
         self.size = download_size
         self.bandwidth = bandwidth
         self.expected_download_duration: float = download_size / bandwidth
-        self.is_finished: bool = False
 
     def start_progress(self) -> None:
         self.start_time = monotonic()
@@ -100,6 +124,7 @@ class MockDownload(DeterminateProgressItem):
         ) / self.expected_download_duration
         if normalized_progress >= 1.0:
             self.is_finished = True
+            self.finish_time = monotonic()
         return min(normalized_progress, 1.0)
 
     def is_completed(self):
@@ -112,36 +137,139 @@ class MockDownload(DeterminateProgressItem):
         return f"{self.__class__.__qualname__}(size={self.size}MB, bandwidth={self.bandwidth}MBps)"
 
 
+class FileDownloadThreaded(DeterminateProgressItem):
+    class DownloadState:
+        total_size: Optional[int] = None
+        downloaded: int = 0.0
+        is_finished: bool = False
+        finish_time: float = -float("inf")
+
+    url: str
+    filename: str
+    download_thread: Thread
+
+    def __init__(self, url: str, filename: str = ""):
+        super().__init__()
+        self.url = url
+        self.filename = filename
+        self.download_state: FileDownloadThreaded.DownloadState = (
+            FileDownloadThreaded.DownloadState()
+        )
+
+    def get_normalized_progress(self) -> float:
+        if self.download_state.total_size is None:
+            return 0.0
+        normalized_progress = (
+            self.download_state.downloaded / self.download_state.total_size
+        )
+        if normalized_progress >= 1.0:
+            self.is_finished = True
+            self.finish_time = monotonic()
+        return min(normalized_progress, 1.0)
+
+    def is_completed(self) -> bool:
+        return self.download_state.is_finished
+
+    def start_progress(self) -> bool:
+        self.start_time = monotonic()
+
+        def download_file(download_state: FileDownloadThreaded.DownloadState):
+            response = requests.get(self.url, stream=True)
+            if response.status_code != 200:
+                raise RuntimeError(
+                    f"downloading {self.url} failed with status code {response.status_code}"
+                )
+            download_state.total_size = (
+                int(response.headers.get("Content-Length", "0")) or None
+            )
+            block_size = 1024
+            with open(self.filename, "wb") as file:
+                for data in response.iter_content(block_size):
+                    download_state.downloaded += len(data)
+                    file.write(data)
+            download_state.is_finished = True
+            download_state.finish_time = monotonic()
+
+        self.download_thread = Thread(
+            name=str(self.id),
+            target=download_file,
+            args=(self.download_state,),
+        )
+        self.download_thread.start()
+        return True
+
+    def cleanup(self):
+        self.download_thread.join()
+
+    def get_progress_item_title(self) -> str:
+        return f"{self.filename} <- {self.url}"
+
+
+@dataclass
+class HeaderPrintState:
+    n_filled: int
+    n_left: int
+    n_jobs_completed: int
+
+    def __iter__(self):
+        yield from (self.n_filled, self.n_left, self.n_jobs_completed)
+
+
 class ProgressBarManager:
     progress_items: tuple[DeterminateProgressItem, ...]
 
     def __init__(self, progress_items: Iterable[DeterminateProgressItem]):
+        self.start_time = None
         self.progress_items = tuple(progress_items)
+        self.header_print_state = HeaderPrintState(
+            0, MAX_NUMBER_CHARACTERS_HEADER_PROGRESS_BAR, 0
+        )
 
     @staticmethod
     def delete_ascii_terminal_line(text_io: TextIO = sys.stdout):
         text_io.write(ANSI_ERASE_CURRENT_LINE)
         text_io.write(ANSI_MOVE_CURSOR_UP_ONE_LINE)
 
-    def pretty_print_progress_bar_header(self, text_io: TextIO):
+    def pretty_print_progress_bar_header(
+        self, text_io: TextIO, update_completed_count=True
+    ):
         n_jobs_total = len(self.progress_items)
-        n_jobs_completed = len(
-            tuple(filter(lambda p: p.is_completed(), self.progress_items))
-        )
+        if update_completed_count:
+            n_jobs_completed = len(
+                tuple(filter(lambda p: p.is_completed(), self.progress_items))
+            )
 
-        n_filled = ceil(
-            n_jobs_completed / n_jobs_total * MAX_NUMBER_CHARACTERS_HEADER_PROGRESS_BAR
-        )
-        n_left = MAX_NUMBER_CHARACTERS_HEADER_PROGRESS_BAR - n_filled
+            n_filled = ceil(
+                n_jobs_completed
+                / n_jobs_total
+                * MAX_NUMBER_CHARACTERS_HEADER_PROGRESS_BAR
+            )
+            n_left = MAX_NUMBER_CHARACTERS_HEADER_PROGRESS_BAR - n_filled
 
-        text_io.write(
-            f'{INDENT}{get_green_bold_colored("Downloading")}  '
-            f'[{"=" * (n_filled - 1) + ">"}{" " * n_left}] [{n_jobs_completed} / {n_jobs_total} downloaded]  \n'
-        )
+            text_io.write(
+                f'{INDENT}{get_green_bold_colored("Downloading")}  '
+                f'[{"=" * (n_filled - 1) + ">"}{" " * n_left}] [{n_jobs_completed} / {n_jobs_total} downloaded]... {format_timespan(monotonic() - self.start_time, detailed=False)}  \n'
+            )
+            self.header_print_state.n_left = n_left
+            self.header_print_state.n_filled = n_filled
+            self.header_print_state.n_jobs_completed = n_jobs_completed
+        else:
+            n_filled, n_left, n_jobs_completed = self.header_print_state
+            text_io.write(
+                f'{INDENT}{get_green_bold_colored("Downloading")}  '
+                f'[{"=" * (n_filled - 1) + ">"}{" " * n_left}] [{n_jobs_completed} / {n_jobs_total} downloaded]... {format_timespan(monotonic() - self.start_time, detailed=False)}  \n'
+            )
 
     def initialize_all_progress_items(self):
         for progress_item in self.progress_items:
             progress_item.start_progress()
+
+    def cleanup_all_progress_items(self):
+        for progress_item in self.progress_items:
+            if not progress_item.is_completed():
+                raise RuntimeError(f"{progress_item} is not completed")
+        for progress_item in self.progress_items:
+            progress_item.cleanup()
 
     def pretty_print_all_progress_items(
         self,
@@ -171,6 +299,7 @@ class ProgressBarManager:
         )
 
     def run(self, text_io: TextIO = sys.stdout):
+        self.start_time = monotonic()
         self.initialize_all_progress_items()
 
         (
@@ -179,31 +308,71 @@ class ProgressBarManager:
         ) = self.get_incomplete_progress_items_state()
         while incomplete_progress_items:
             text_io.write(ANSI_HIDE_CURSOR)
-            self.pretty_print_progress_bar_header(text_io)
             while (
                 monotonic() - incomplete_progress_items_update_time
                 < COMPLETED_JOBS_REFRESH_TIME
             ):
+                self.pretty_print_progress_bar_header(
+                    text_io, update_completed_count=False
+                )
                 self.pretty_print_all_progress_items(incomplete_progress_items, text_io)
+                self.delete_ascii_terminal_line()
 
             (
                 incomplete_progress_items,
                 incomplete_progress_items_update_time,
             ) = self.get_incomplete_progress_items_state()
-            self.delete_ascii_terminal_line()
+
         self.pretty_print_progress_bar_header(text_io)
-        self.delete_ascii_terminal_line()
-        text_io.write(ANSI_ERASE_CURRENT_LINE)
-        text_io.write(get_green_bold_colored(f"{INDENT}Completed"))
-        text_io.write(ANSI_SHOW_CURSOR)
+        self.cleanup_all_progress_items()
+        text_io.write(
+            f"Successfully finished in {format_timespan(monotonic() - self.start_time)}"
+        )
+        sys.stdout.write(ANSI_SHOW_CURSOR)
+
+
+@click.command(
+    name="Progress",
+    help="This is a program to illustrate how to build helpful progress reports for CLIs",
+)
+@click.option(
+    "--real-downloads",
+    "-r",
+    is_flag=True,
+    default=False,
+    help="Use real downloads from https://testfiledownload.com/ to do test the program",
+)
+@click.option(
+    "--num-items", "-n", default=8, help="Number of items to run download concurrently"
+)
+@click.option("--download-speed", "-d", default=20, help="A mock download speed")
+@click.option(
+    "--max-mock-file-size",
+    "-m",
+    default=500,
+    help="Mock download files are generated from a uniform distribution [1, max_mock_file_size)",
+)
+def test(
+    real_downloads: bool, num_items: int, download_speed: int, max_mock_file_size: int
+):
+    if real_downloads:
+        small_file_url = "https://speed.hetzner.de/100MB.bin"
+        downloads = [
+            FileDownloadThreaded(small_file_url, f"large_file_{i}.bin")
+            for i in range(num_items)
+        ]
+    else:
+        downloads = [
+            MockDownload(randint(1, max_mock_file_size), download_speed / num_items)
+            for _ in range(num_items)
+        ]
+    progress_bar_manager = ProgressBarManager(downloads)
+    progress_bar_manager.run()
 
 
 if __name__ == "__main__":
-    DOWNLOAD_SPEED = 100  # 10MB per sec
-    NUMBER_OF_ITEMS = 10
-    downloads = [
-        MockDownload(randint(1, 100), DOWNLOAD_SPEED / NUMBER_OF_ITEMS)
-        for _ in range(NUMBER_OF_ITEMS)
-    ]
-    progress_bar_manager = ProgressBarManager(downloads)
-    progress_bar_manager.run()
+    try:
+        test()
+    except KeyboardInterrupt:
+        sys.stdout.write(ANSI_SHOW_CURSOR)
+        sys.exit(0)
